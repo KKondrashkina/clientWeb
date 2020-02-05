e5d1b062aaf27b1c295eca972e42ad1f0d95a75a
Ekaterina <Ekaterina@DESKTOP-KVLSQMK>
2020-02-05 19:35:24 +0700

    Добавлен новый функционал в Todo List.

--- ClientWeb/ToDoList/ToDoScript.js
+++ ClientWeb/ToDoList/ToDoScript.js
@@ -3,12 +3,12 @@
 document.addEventListener("DOMContentLoaded", function () {
     var addTodo = document.getElementById("add-todo");
 
-    addTodo.addEventListener("click", function (e1) {
+    addTodo.addEventListener("click", function () {
         var todoText = document.getElementById("todo-text");
 
         if (todoText.value === "") {
             var blink = setInterval(() => {
-                todoText.setAttribute("style", "background-color: #424347;");
+                todoText.setAttribute("style", "background-color: #424347");
 
             }, 100);
             setTimeout(() => {
@@ -25,11 +25,11 @@ document.addEventListener("DOMContentLoaded", function () {
         todoList.appendChild(newTodoTr);
         newTodoTr.setAttribute("class", "hide");
 
-        newTodoTr.addEventListener("mousemove", function (e2) {
+        newTodoTr.addEventListener("mousemove", function () {
             newTodoTr.removeAttribute("class");
         });
 
-        newTodoTr.addEventListener("mouseout", function (e3) {
+        newTodoTr.addEventListener("mouseout", function () {
             newTodoTr.setAttribute("class", "hide");
         });
 
@@ -54,7 +54,7 @@ document.addEventListener("DOMContentLoaded", function () {
         deleteButton.setAttribute("type", "button");
         deleteButton.textContent = "╳";
 
-        deleteButton.addEventListener("mousedown", function (e4) {
+        deleteButton.addEventListener("mousedown", function () {
             newTodoTr.parentNode.removeChild(newTodoTr);
         });
 
@@ -68,7 +68,79 @@ document.addEventListener("DOMContentLoaded", function () {
         newTodoText.innerText = todoText.value;
         newTodoTr.appendChild(newTodoText);
 
-        doneLabel.addEventListener("mousedown", function (e5) {
+        newTodoText.addEventListener("mousedown", function () {
+            var oldText = newTodoText.textContent;
+            var parent = newTodoText.parentNode;
+
+            parent.removeChild(newTodoText);
+
+            var edit = document.createElement("input");
+
+            edit.value = oldText;
+
+            edit.setAttribute("type", "text");
+            edit.setAttribute("class", "todo-text");
+            edit.setAttribute("style", "background-color: #424347");
+
+            parent.appendChild(edit);
+
+            newTodoDone.removeChild(doneLabel);
+            newTodoDelete.removeChild(deleteButton);
+
+            var yes = document.createElement("button");
+
+            yes.setAttribute("type", "button");
+            yes.textContent = "✓";
+
+            var no = document.createElement("button");
+
+            no.setAttribute("type", "button");
+            no.textContent = "╳";
+
+            yes.addEventListener("mousedown", function () {
+                if (edit.value === "") {
+                    var editBlink = setInterval(() => {
+                        edit.setAttribute("style", "background-color: #626367");
+
+                    }, 100);
+                    setTimeout(() => {
+                        clearInterval(editBlink);
+                        edit.setAttribute("style", "background-color: #424347");
+                    }, 1000);
+
+                    return;
+                }
+
+                newTodoText.textContent = edit.value;
+
+                parent.removeChild(edit);
+                parent.appendChild(newTodoText);
+
+                newTodoDone.removeChild(yes);
+                newTodoDelete.removeChild(no);
+
+                newTodoDone.appendChild(doneLabel);
+                newTodoDelete.appendChild(deleteButton);
+            });
+
+            no.addEventListener("mousedown", function () {
+                newTodoText.textContent = oldText;
+
+                parent.removeChild(edit);
+                parent.appendChild(newTodoText);
+
+                newTodoDone.removeChild(yes);
+                newTodoDelete.removeChild(no);
+
+                newTodoDone.appendChild(doneLabel);
+                newTodoDelete.appendChild(deleteButton);
+            });
+
+            newTodoDone.appendChild(yes);
+            newTodoDelete.appendChild(no);
+        });
+
+        doneLabel.addEventListener("mousedown", function () {
             if (!doneCheckbox.checked) {
                 newTodoText.setAttribute("class", "done");
             } else {
