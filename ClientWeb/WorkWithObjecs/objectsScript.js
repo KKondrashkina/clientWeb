a18145e1db1751a73d7592781f69a947e52ff208
Ekaterina <Ekaterina@DESKTOP-KVLSQMK>
2020-02-07 13:45:31 +0700

    Исправлены замечания в WorkWithObjects

--- ClientWeb/WorkWithObjecs/objectsScript.js
+++ ClientWeb/WorkWithObjecs/objectsScript.js
@@ -1,101 +1,99 @@
-﻿"use strict";
+﻿(function () {
+    "use strict";
 
-var countries = [
-    {
-        countryName: "New Zealand",
-        cities: [
-            {
-                cityName: "Auckland",
-                population: 1534700
-            },
-            {
-                cityName: "Wellington",
-                population: 412500
-            },
-            {
-                cityName: "Hamilton",
-                population: 203400
-            }
-        ]
-    },
-    {
-        countryName: "Republic of Peru",
-        cities: [
-            {
-                cityName: "Arequipa",
-                population: 1008290
-            },
-            {
-                cityName: "Trujillo",
-                population: 919899
-            }
-        ]
-    },
-    {
-        countryName: "Syrian Arab Republic",
-        cities: [
-            {
-                cityName: "Aleppo",
-                population: 2132100
-            },
-            {
-                cityName: "Damascus",
-                population: 1754000
-            },
-            {
-                cityName: "Homs",
-                population: 900492
-            }
-        ]
-    },
-    {
-        countryName: "Vietnam",
-        cities: [
-            {
-                cityName: "Ho Chi Minh City",
-                population: 8426000
-            },
-            {
-                cityName: "Haiphong",
-                population: 1837302
-            },
-            {
-                cityName: "Danang",
-                population: 887069
-            }
-        ]
-    }
-];
-
-function getСountriesWithMaxCitiesCount(array) {
-    var maxCityCount = 0;
-    var cityCount;
-
-    array.forEach(function (e) {
-        cityCount = e.cities.length;
-        if (maxCityCount < cityCount) {
-            maxCityCount = cityCount;
+    var countries = [
+        {
+            countryName: "New Zealand",
+            cities: [
+                {
+                    cityName: "Auckland",
+                    population: 1534700
+                },
+                {
+                    cityName: "Wellington",
+                    population: 412500
+                },
+                {
+                    cityName: "Hamilton",
+                    population: 203400
+                }
+            ]
+        },
+        {
+            countryName: "Republic of Peru",
+            cities: [
+                {
+                    cityName: "Arequipa",
+                    population: 1008290
+                },
+                {
+                    cityName: "Trujillo",
+                    population: 919899
+                }
+            ]
+        },
+        {
+            countryName: "Syrian Arab Republic",
+            cities: [
+                {
+                    cityName: "Aleppo",
+                    population: 2132100
+                },
+                {
+                    cityName: "Damascus",
+                    population: 1754000
+                },
+                {
+                    cityName: "Homs",
+                    population: 900492
+                }
+            ]
+        },
+        {
+            countryName: "Vietnam",
+            cities: [
+                {
+                    cityName: "Ho Chi Minh City",
+                    population: 8426000
+                },
+                {
+                    cityName: "Haiphong",
+                    population: 1837302
+                },
+                {
+                    cityName: "Danang",
+                    population: 887069
+                }
+            ]
         }
-    });
+    ];
 
-    return array.filter(function (e) {
-        return e.cities.length === maxCityCount;
-    });
-}
+    function getCountriesWithMaxCitiesCount(array) {
+        var maxCitiesCount = array.reduce(function (memo, e) {
+            return (memo.cities.length > e.cities.length ? memo : e);
+        }).cities.length;
 
-console.log("Страны с максимальным количеством городов:");
-console.log(getСountriesWithMaxCitiesCount(countries));
+        return array.filter(function (e) {
+            return e.cities.length === maxCitiesCount;
+        });
+    }
 
-function getCountryPopulation(array) {
-    return array.map(function (country) {
-        return {
-            name: country.countryName,
-            populaion: country.cities.reduce(function (memo, city) {
-                return memo + city.population;
-            }, 0)
-        };
-    });
-}
+    console.log("Страны с максимальным количеством городов:");
+    console.log(getCountriesWithMaxCitiesCount(countries));
 
-console.log("Страны и их суммрная численность:");
-console.log(getCountryPopulation(countries));
+    function getCountryPopulation(array) {
+        var countriesPopulation = {};
+
+        array.forEach(function (country) {
+            countriesPopulation[country.countryName] =
+                country.cities.reduce(function (memo, city) {
+                    return memo + city.population;
+                }, 0);
+        });
+
+        return countriesPopulation;
+    }
+
+    console.log("Страны и их суммрная численность:");
+    console.log(getCountryPopulation(countries));
+})();
