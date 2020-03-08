36e2a077e176806c91ef418920486d47437c9909
Екатерина Кондрашкина <kkondrashkina@gmail.com>
2020-03-09 01:08:49 +0700

    Исправлены замечания в Underscore

--- ClientWeb/UnderscoreProject/peopleListFiltering.js
+++ ClientWeb/UnderscoreProject/peopleListFiltering.js
@@ -54,23 +54,28 @@
         }
     ];
 
-    var averageAge = _.chain(people)
+    var averageAge = (_.chain(people)
         .pluck("age")
-        .reduce(function (memo, value, i, people) { return memo + value / people.length; }, 0)
-        .value();
+        .reduce(function (memo, value) {
+            return memo + value;
+        }, 0)
+        .value()) / people.length;
 
     console.log("Средний возраст людей из списка = " + averageAge);
 
     var middleAgedPeople = _.chain(people)
-        .filter(function (people) { return people.age >= 20 && people.age <= 30 })
+        .filter(function (people) {
+            return people.age >= 20 && people.age <= 30;
+        })
         .sortBy("age")
         .value();
 
     console.log("Люди от 20 до 30 лет, упорядоченные по возрастанию возраста: ");
     console.log(middleAgedPeople);
 
-    _.chain(people)
-        .each(function (people) { return people.fullName = people.lastName + " " + people.name });
+    _.each(people, function (people) {
+        return people.fullName = people.lastName + " " + people.name;
+    });
 
     console.log("Список людей, после добавления нового поля:");
     console.log(people);
